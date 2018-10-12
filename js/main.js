3264cfdb3300bca2b5fc346fcdb93c999eb3a626
Siddiqui <Omer.Siddiqui@CenturyLink.com>
2018-10-12 15:24:30 -0600

    changed input fields for additionalInputs

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var sourceSystem = $("#sourceSystem"),
     addRow = $(".addRow");
 
     const addInputRow = () => {
-        var item = `<li id="1" class="moreInfo" style="display: flex; justify-content: center; margin-top: 3px"><div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png" alt="delete button"></button><button class="addRow"><img src="assets/add-icon.png" alt="add button"></button></div><input placeholder="City" type="text"><input placeholder="State" type="text"><input placeholder="Location ID" type="text"><input type="submit"></li>`;
+        var item = `<li id="1" class="moreInfo" style="display: flex; justify-content: center; margin-top: 3px"><div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png" alt="delete button"></button><button class="addRow"><img src="assets/add-icon.png" alt="add button"></button></div><select name="product"id="product"><option value="test1">test1</option><option value="test2">test2</option><option value="test3">test3</option><option value="test4">test4</option></select><select name="bandwidth"id="bandwidth"><option value="MBPS">MBPS</option><option value="GBPS">GBPS</option></select><select name="activity"id="activity"><option value="new">New</option><option value="change">Change</option><option value="modify">Modify</option></select><select name="onNet"id="onNet"><option value="onNet">On Net</option><option value="offNet">Off Net</option></select><div id="wiringCheckbox"><input type="checkbox"id="insideWiring"name="insideWiring"value="insideWiring"><label for="subscribeNews">Inside wiring?</label></div></li></ul>`;
         $(".additionalInputs").append(item);
     }
 
@@ -21,7 +21,7 @@ var sourceSystem = $("#sourceSystem"),
 
     $(".additionalInputs").on("click", "li .addRow", function(e) {
         e.stopPropagation();
-        var item = `<li id=${id} class="moreInfo animated fadeInDown" style="display: flex; justify-content: center; margin-top: 3px"><div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png" alt="delete button"></button><button class="addRow"><img src="assets/add-icon.png" alt="add button"></button></div><input placeholder="City" type="text"><input placeholder="State" type="text"><input placeholder="Location ID" type="text"><input type="submit"></li>`;
+        var item = `<li id=${id} class="moreInfo animated fadeInDown" style="display: flex; justify-content: center; margin-top: 3px"><div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png"alt="delete button"></button><button class="addRow"><img src="assets/add-icon.png"alt="add button"></button></div><select name="product"id="product"><option value="test1">test1</option><option value="test2">test2</option><option value="test3">test3</option><option value="test4">test4</option></select><select name="bandwidth"id="bandwidth"><option value="MBPS">MBPS</option><option value="GBPS">GBPS</option></select><select name="activity"id="activity"><option value="new">New</option><option value="change">Change</option><option value="modify">Modify</option></select><select name="onNet"id="onNet"><option value="onNet">On Net</option><option value="offNet">Off Net</option></select><div id="wiringCheckbox"><input type="checkbox"id="insideWiring"name="insideWiring"value="insideWiring"><label for="subscribeNews">Inside wiring?</label></div></li></ul>`;
         $(".additionalInputs").append(item);
         id++;
     })
@@ -57,3 +57,5 @@ var sourceSystem = $("#sourceSystem"),
         });
             
     })
+
+
