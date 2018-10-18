13bd9ed0c9e448a505284cfa0193037bfdadd9c8
Siddiqui <Omer.Siddiqui@CenturyLink.com>
2018-10-18 12:45:30 -0600

    additional inputs as rows

--- js/main.js
+++ js/main.js
@@ -4,25 +4,113 @@ var sourceSystem = $("#sourceSystem"),
     modalWindow = $("#modalWindow"),
     modalOpen = $("#moreDetailsBtn"),
     modalClose = $("#modal-close"),
+    additionalInputs = $(".additionalInputs"),
     deleteRow = $(".deleteRow"),
     addRow = $(".addRow");
 
+    // to add the first row with an ID of 1
     const addInputRow = () => {
-        var item = `<li id="1" class="moreInfo" style="display: flex; justify-content: center; margin-top: 3px"><div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png" alt="delete button"></button><button class="addRow"><img src="assets/add-icon.png" alt="add button"></button></div><select name="product"id="product"><option value="test1">test1</option><option value="test2">test2</option><option value="test3">test3</option><option value="test4">test4</option></select><select name="bandwidth"id="bandwidth"><option value="MBPS">MBPS</option><option value="GBPS">GBPS</option></select><select name="activity"id="activity"><option value="new">New</option><option value="change">Change</option><option value="modify">Modify</option></select><select name="onNet"id="onNet"><option value="onNet">On Net</option><option value="offNet">Off Net</option></select><div id="wiringCheckbox"><input type="checkbox"id="insideWiring"name="insideWiring"value="insideWiring"><label for="subscribeNews">Inside wiring?</label></div></li></ul>`;
-        $(".additionalInputs").append(item);
+        var item = `<li id="1" class="moreInfo" style="display: flex; justify-content: center; margin-top: 3px">
+        <div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png" alt="delete button"></button><button
+                class="addRow"><img src="assets/add-icon.png" alt="add button"></button></div>
+        <div class="additionalInputsTopRow">
+            <div class="inputItem">
+                <label for="product">Product: </label>
+                <select name="product" id="product">
+                    <option value="test1">test1</option>
+                    <option value="test2">test2</option>
+                    <option value="test3">test3</option>
+                    <option value="test4">test4</option>
+                </select>
+            </div>
+            <div class="inputItem">
+                <label for="bandwidth">Bandwidth: </label>
+                <select name="bandwidth" id="bandwidth">
+                    <option value="MBPS">MBPS</option>
+                    <option value="GBPS">GBPS</option>
+                </select>
+            </div>
+        </div>
+        <div class="additionalInputsBottomRow">
+            <div class="inputItem">
+                <label for="activity">Activity: </label>
+                <select name="activity" id="activity">
+                    <option value="new">New</option>
+                    <option value="change">Change</option>
+                    <option value="modify">Modify</option>
+                </select>
+            </div>
+            <div class="inputItem">
+                <label for="net">Net: </label>
+                <select name="onNet" id="onNet">
+                    <option value="onNet">On Net</option>
+                    <option value="offNet">Off Net</option>
+                </select>
+            </div>
+        </div>
+        <div id="wiringCheckbox"><input type="checkbox" id="insideWiring" name="insideWiring" value="insideWiring"><label
+                for="subscribeNews">Inside wiring?</label></div>
+        <h4 class="locationId">Location #1</h4>
+    </li>`;
+        additionalInputs.append(item);
     }
 
-    $(".additionalInputs").on("click", "li .deleteRow", function(e) {
+    // to delete individual rows of additional inputs
+    additionalInputs.on("click", "li .deleteRow", function(e) {
         e.stopPropagation();
         $(this).parent().parent().addClass("animated fadeOutLeft").fadeOut("slow");
     })
 
+    
+
+    // to add additional rows with incrementing numbers for each row's ID
     let id = 2;
 
-    $(".additionalInputs").on("click", "li .addRow", function(e) {
+    additionalInputs.on("click", "li .addRow", function(e) {
         e.stopPropagation();
-        var item = `<li id=${id} class="moreInfo animated fadeInDown" style="display: flex; justify-content: center; margin-top: 3px"><div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png"alt="delete button"></button><button class="addRow"><img src="assets/add-icon.png"alt="add button"></button></div><select name="product"id="product"><option value="test1">test1</option><option value="test2">test2</option><option value="test3">test3</option><option value="test4">test4</option></select><select name="bandwidth"id="bandwidth"><option value="MBPS">MBPS</option><option value="GBPS">GBPS</option></select><select name="activity"id="activity"><option value="new">New</option><option value="change">Change</option><option value="modify">Modify</option></select><select name="onNet"id="onNet"><option value="onNet">On Net</option><option value="offNet">Off Net</option></select><div id="wiringCheckbox"><input type="checkbox"id="insideWiring"name="insideWiring"value="insideWiring"><label for="subscribeNews">Inside wiring?</label></div></li></ul>`;
-        $(".additionalInputs").append(item);
+        var item = `<li id=${id} class="moreInfo animated fadeInDown" style="display: flex; justify-content: center; margin-top: 3px">
+        <div class="add-delete"><button class="deleteRow"><img src="assets/delete-icon.png" alt="delete button"></button><button
+                class="addRow"><img src="assets/add-icon.png" alt="add button"></button></div>
+        <div class="additionalInputsTopRow">
+            <div class="inputItem">
+                <label for="product">Product: </label>
+                <select name="product" id="product">
+                    <option value="test1">test1</option>
+                    <option value="test2">test2</option>
+                    <option value="test3">test3</option>
+                    <option value="test4">test4</option>
+                </select>
+            </div>
+            <div class="inputItem">
+                <label for="bandwidth">Bandwidth: </label>
+                <select name="bandwidth" id="bandwidth">
+                    <option value="MBPS">MBPS</option>
+                    <option value="GBPS">GBPS</option>
+                </select>
+            </div>
+        </div>
+        <div class="additionalInputsBottomRow">
+            <div class="inputItem">
+                <label for="activity">Activity: </label>
+                <select name="activity" id="activity">
+                    <option value="new">New</option>
+                    <option value="change">Change</option>
+                    <option value="modify">Modify</option>
+                </select>
+            </div>
+            <div class="inputItem">
+                <label for="net">Net: </label>
+                <select name="onNet" id="onNet">
+                    <option value="onNet">On Net</option>
+                    <option value="offNet">Off Net</option>
+                </select>
+            </div>
+        </div>
+        <div id="wiringCheckbox"><input type="checkbox" id="insideWiring" name="insideWiring" value="insideWiring"><label
+                for="subscribeNews">Inside wiring?</label></div>
+        <h4 class="locationId">Location #${id}</h4>
+    </li>`;
+        additionalInputs.append(item);
         id++;
     })
 
@@ -32,6 +120,7 @@ var sourceSystem = $("#sourceSystem"),
     //   document ready
     $(document).ready(function() {
 
+        // more details pop up
         modalWindow.hide();
 
         modalOpen.on("click", function() {
@@ -42,17 +131,22 @@ var sourceSystem = $("#sourceSystem"),
             modalWindow.hide();
         })
 
+
+        // if source system is not supported, provide additional fields
         sourceSystem.change(function(){
             if(!($(this).val() == 'supported' || $(this).val() == "")) {
               addInputRow();
+              additionalInputs.css("border", "1px solid rebeccapurple");
+              // System Message is error message
               $("#systemMessage").text("Order Not Available").css("color", "red");
             }
         });
 
+        // if source system is supported or empty, empty additional inputs field
         sourceSystem.change(function(){
             if(($(this).val() == 'supported' || $(this).val() == "")) {
               $("#systemMessage").text("Order Found").css("color", "green");
-              $(".additionalInputs").empty();
+              additionalInputs.empty();
             }
         });
             
